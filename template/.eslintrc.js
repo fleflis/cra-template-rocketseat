d2bb57d5642c7122abc32584c398a536823d72d6
Christian Sanches <christian@endless-point.org>
2020-02-26 09:28:23 -0300

    initial commit

---
+++ template/.eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier' : 'error',
+    'import/prefer-default-export': 'off',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions : ['.jsx', '.js']}
+		],
+  },
+};
---
+++ template/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GlobalStyles from './global/styles';
+import Index from './pages/Index';
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Index />
+    </>
+  );
+};
+
+export default App;
